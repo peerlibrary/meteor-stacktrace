bab23f2aa5bcd80f75e4f6c3c8bc2a5dd3f0e590
Mitar <mitar.git@tnode.com>
2014-09-08 13:59:25 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,21 +1,10 @@
 Package.describe({
   summary: "Meteor package for getting uniform stack traces in all web browsers and server",
   version: '0.1.3',
-  name: 'peerlibrary:stacktrace',
+  name: 'mrt:stacktrace',
   git: 'https://github.com/peerlibrary/meteor-stacktrace.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom('METEOR@0.9.1');
-  api.export('StackTrace');
-
-  api.add_files([
-    'stacktrace/stacktrace.js',
-    'export-stacktrace.js'
-  ], ['client', 'server']);
-});
-
-Package.on_test(function (api) {
-  api.use(['peerlibrary:stacktrace', 'tinytest', 'test-helpers', 'underscore'], ['client', 'server']);
-  api.add_files('tests.js', ['client', 'server']);
+  api.imply('peerlibrary:stacktrace@0.1.3');
 });
